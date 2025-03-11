a8dfe00d6f358f3ff9c0bdd95824b54fc9114d2b
mpkhonde <91606609+mpkhonde@users.noreply.github.com>
2025-03-11 23:08:07 +0100

    Inital commit

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,29 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have the 'FR_Lektion_9_Uppgifter' title`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('FR_Lektion_9_Uppgifter');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, FR_Lektion_9_Uppgifter');
+  });
+});
---
+++ src/app/app.config.ts
@@ -0,0 +1,13 @@
+import { ApplicationConfig } from "@angular/core";
+import { provideHttpClient, withFetch } from "@angular/common/http";
+import { provideRouter } from "@angular/router";
+import { routes } from './app.routes';
+import { provideZoneChangeDetection } from "@angular/core";
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter( routes),
+    provideHttpClient(withFetch()),
+  ],
+};
